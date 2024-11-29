ef3931b709057570eab64988472f10d7ca103691
An12iket <Aniketchavan121012@gmail.com>
2024-11-29 10:58:19 +0530

    added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import "./app.scss";
-import AcUnitIcon from '@mui/icons-material/AcUnit';
-import Home from "./home/Home"
+import Home from './home/Home';
 
 const App = () => {
   return (
     <div>
       <Home/>
-     <AcUnitIcon/>
     </div>
   )
 }
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,9 +1,68 @@
+import Navbar from "../components/navbar/Navbar";
 import "./home.scss";
 
 const Home = () => {
   return (
     <div className='home'>
-      Hello
+      <Navbar/>
+      <img 
+      width="100%" 
+      src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+      alt="profile img" />
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, delectus? Corporis impedit adipisci esse aliquam aspernatur magnam est unde iure ipsam, nulla voluptatem ullam, similique vero deleniti eius perferendis quam.
     </div>
   )
 }
