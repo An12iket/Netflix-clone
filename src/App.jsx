f552c745e6aa251f08040e98aae8bbec03be5a67
An12iket <Aniketchavan121012@gmail.com>
2024-11-29 10:03:52 +0530

    added saas embedded

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import "./app.scss";
 import AcUnitIcon from '@mui/icons-material/AcUnit';
+import Home from "./home/Home"
 
 const App = () => {
   return (
     <div>
+      <Home/>
      <AcUnitIcon/>
     </div>
   )
